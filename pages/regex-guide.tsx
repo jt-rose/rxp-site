c8ddfa478c561b44ccfc706b859602ddeb86d0b4
Jeffrey Rose <jtr219@outlook.com>
2020-11-12 13:51:17 -0500

    added table styling

--- pages/regex-guide.tsx
+++ pages/regex-guide.tsx
@@ -9,47 +9,80 @@ const RegexGuidePage = () => (
     <p>A quick reference guide to common regex characters.</p>
     <h2>Regex Behavior Modifiers</h2>
     <table>
-      <td>
-        Symbol
-      </td>
-      <td>
-        Name
-      </td>
-      <td>
-        Description
-      </td>
-      <td>
-        Example
-      </td>
+      <thead>
+        <tr>
+        <th>Symbol</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Example</th>
+        </tr>
+      </thead>
+      <tbody>
       {regexData.map(data => (
         <tr>
-          <th>{data.symbol}</th>
-          <th>{data.name}</th>
-          <th>{data.description}</th>
-          <th>{data.example}</th>
+          <td>{data.symbol}</td>
+          <td>{data.name}</td>
+          <td>{data.description}</td>
+          <td>{data.example}</td>
         </tr>
       ))}
+      </tbody>
     </table>
     <br />
     <h2>Special Characters</h2>
     <table>
-    <td>
-        Symbol
-      </td>
-      <td>
-        Name
-      </td>
-      <td>
-        Description
-      </td>
+      <thead>
+        <tr>
+        <th>Symbol</th>
+      <th>Name</th>
+      <th>Description</th>
+        </tr>
+      </thead>
+    <tbody>
       {specialCharData.map(data => (
         <tr>
-        <th>{data.symbol}</th>
-        <th>{data.name}</th>
-        <th>{data.description}</th>
+        <td>{data.symbol}</td>
+        <td>{data.name}</td>
+        <td>{data.description}</td>
       </tr>
       ))}
+      </tbody>
     </table>
+      <style jsx>{`
+        table {
+          border-collapse: collapse;
+          margin: 25px;
+          font-size: 0.9em;
+          font-damily: sans-serif;
+          min-width: 400px;
+          box-shadow: 0 0 20px rgba(0, 0, 0, 0.15)
+        }
+        thead tr {
+          background-color: #009879;
+          color: #ffffff;
+          text-align: left;
+        }
+        th, td {
+          padding: 12px 15px;
+        }
+       tbody tr {
+          border-bottom: 1px solid #dddddd;
+      }
+      
+      tbody tr:nth-of-type(even) {
+          background-color: #f3f3f3;
+      }
+      
+      tbody tr:last-of-type {
+          border-bottom: 2px solid #009879;
+      }
+      tbody tr:hover {
+        font-weight: bold;
+        color: #009879;
+    }
+    
+      
+      `}</style>
   </Layout>
 );
 

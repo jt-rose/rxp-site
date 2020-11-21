df40317baf0dbe5c5f9d925f3d5ec7f1258f857b
Jeffrey Rose <jtr219@outlook.com>
2020-11-21 16:03:45 -0500

    added import guide and code styling

--- pages/guide.tsx
+++ pages/guide.tsx
@@ -21,22 +21,42 @@ const GuideSection = ({children}: Props) => (
         text-align: center;
         margin: 0;
       }
+      .guide-section > :global(p .code-in-text) {
+        color: #353535;
+        background-color: #f5f2f0;
+        border-radius: 3px;
+        padding: .2em .5em 0;
+      }
     `}
     </style>
   </div>
 );
 
+const ImportGuide = () => (
+  <GuideSection>
+    <h2 className="section-title">Import the Library</h2>
+    <p>RXP can be installed directly through NPM / Yarn:</p>
+    <CodeSample sample={`npm i rxp
+yarn add rxp`} />
+    <p>Once installed, the RXP object can be imported, which contains every aspect of the library</p>
+    <CodeSample sample={`import { RXP } from "rxp"
+const { init, optional, either } = RXP;`} />
+<p>If you want to install specific functions directly, you can specify them in the import:</p>
+<CodeSample sample={`import { init, optional, either } from "rxp"`} />
+  </GuideSection>
+)
+
 const InitGuide = () => (
   <GuideSection>
-  <h2 className="section-title">Initialize the constructor</h2>
-    <p>The RXP 'init' function works by accepting text to search for, creating an object 
+  <h2 className="section-title">Initialize the Constructor</h2>
+    <p>The RXP <code className="code-in-text">init</code> function works by accepting text to search for, creating an object 
       that provides methods to modify the search behavior, and then converts it to a 
-      standard regex through the ‘construct’ command.</p>
+      standard regex through the <code className="code-in-text">construct</code> command.</p>
 
       <CodeSample sample={`init("sample").construct() // 	/sample/
 init("sample").atStart.construct() // 	/^(?:sample)/`} />
       <p>
-      The ‘init’ function combines any number of arguments into a text to search for:
+      The <code className="code-in-text">init</code> function combines any number of arguments into a text to search for:
       </p>
       <CodeSample sample={`init("search", "for", "me").construct() // /search for me/`} />
     
@@ -46,7 +66,7 @@ init("sample").atStart.construct() // 	/^(?:sample)/`} />
       </p>
       <CodeSample sample={`init("() and []").construct() // /\\(\\) and \\[\\]/`} />
       <p>
-      Init can also accept regex or other RXP constructor objects for 
+      <code className="code-in-text">init</code> can also accept regex or other RXP constructor objects for 
       maximum flexibility without further escaping them:
       </p>
       <CodeSample sample={`const sample = init("sample");
@@ -67,7 +87,7 @@ const patternsAndPatternsOhMy = init(
 
 const ModifyTextGuide = () => (
   <GuideSection>
-    <h2 className="section-title">Modify Search Behavior through RXP Methods</h2>
+    <h2 className="section-title">Modify Regex Behavior</h2>
       <p>
       After creating the RXP constructor object, 
       the provided text can be modified with regex behavior:
@@ -90,11 +110,11 @@ sample.isOptional.construct() // /(?:sample)?/`} />
 sample.occursOnceOrMore.construct() // /(?:sample)+?/ 
 sample.occursOnceOrMore.and.isGreedy.construct() // /(?:sample)+/`} />
       <p>
-      Regex variables can be defined with the ‘isVariable’ method and 
-      then passed to other RXP constructors through init as often as needed. 
-      When using the ‘construct’ method, the variables will be rewritten in 
+      Regex variables can be defined with the <code className="code-in-text">isVariable</code> method and 
+      then passed to other RXP constructors through <code className="code-in-text">init</code> as often as needed. 
+      When using the <code className="code-in-text">construct</code> method, the variables will be rewritten in 
       order of appearance to work as expected. Here is an example using the 
-      preset anyDigit (covered below):
+      preset <code className="code-in-text">anyDigit</code> (covered below):
       </p>
       <CodeSample sample={`const regexVar = anyDigit.occurs(3).and.isVariable("var");
 init(regexVar, " with ", regexVar).construct() // /(?<var>d{3}) with //k<var>/`}/>
@@ -105,12 +125,12 @@ const ConstructGuide = () => (
   <GuideSection>
     <h2 className="section-title">Convert to Regex</h2>
       <p>
-      The ‘construct’ function can be passed arguments to define the search flags:
+      The <code className="code-in-text">construct</code> function can be passed arguments to define the search flags:
       </p>
       <CodeSample sample={`sample.construct("g") // /sample/g 
 sample.construct("global", "s", "I") // /sample/gsi`} />
       <p>
-      If init is passed a regex argument including a flag, the flag will 
+      If <code className="code-in-text">init</code> is passed a regex argument including a flag, the flag will 
       be stripped from it and would need to be added back with construct:
       </p>
       <CodeSample sample={`init(/search/g, " for me").construct() // /search for me/`} />
@@ -121,20 +141,23 @@ const PresetsGuide = () => (
   <GuideSection>
     <h2 className="section-title">Presets</h2>
       <p>
-      To use special characters, such as \d to match a digit or \w to 
-      match a letter, you can use the ‘presets’ provided with RXP, such 
-      as anyDigit or anyLetter. These are preloaded RXP constructor 
+      To use special characters, such as <code className="code-in-text">\d</code> to match a digit or <code className="code-in-text">\w</code> to 
+      match a letter, you can use the <code className="code-in-text">presets</code> provided with RXP, such 
+      as <code className="code-in-text">anyDigit</code> or <code className="code-in-text">anyLetter</code>. These are preloaded RXP constructor 
       objects with all the functionality built in:
       </p>
-      <CodeSample sample={`anyDigit.occurs(3).construct() // /(?:[0123456789])3/ 
-anyLetter.atEnd.construct() // /(?:\w)$/ equiv`} />
+      <CodeSample sample={`anyDigit.occurs(3).construct() // /(?:[0123456789]){3}/ 
+anyLetter.atEnd.construct() // /(?:\\w)$/`} />
       <p>
-      Since the ‘init’ function will automatically escape special 
+      Since the <code className="code-in-text">init</code> function will automatically escape special 
       characters, if you want to pass them in directly without using 
       presets then you would need to use a standard regex:
       </p>
-      <CodeSample sample={`init("\\d").construct // XXX /\\\\d/
-init(/\\d/).construct() // good /\\d/`} />
+      <CodeSample sample={`// incorrect:
+init("\\d").construct // result: /\\\\d/
+
+// correct:
+init(/\\d/).construct() // result: /\\d/`} />
       <p>
       A full list of presets can be found on the <Link href="/api-guide#presets">API page</Link>.
       </p>
@@ -145,10 +168,10 @@ const ShorthandsGuide = () => (
   <GuideSection>
     <h2 className="section-title">Shorthands</h2>
       <p>
-      ‘Shorthands’ are functions that create an RXP object and 
+      <code className="code-in-text">shorthands</code> are a group of functions that create an RXP object and 
       immediately apply a desired search behavior. These are provided 
       to improve readability, and there is no functional difference 
-      between optional(“text”) and init(“text”).isOptional.
+      between something like <code className="code-in-text">optional(“text”)</code> and <code className="code-in-text">init(“text”).isOptional</code>.
       </p>
       <CodeSample sample={`init(
     optional("("), 
@@ -158,14 +181,15 @@ const ShorthandsGuide = () => (
       <p>
       A variety of shorthands are available and can be found on the <Link href="/api-guide#shorthands">API page</Link>. 
       All of these produce the standard RXP constructor object with the unique 
-      exceptions of ‘withBoundaries’ and ‘wrapRXP’.
+      exceptions of <code className="code-in-text">withBoundaries</code> and <code className="code-in-text">wrapRXP</code>.
       </p>
   </GuideSection>
 );
 
 const GuidePage = () => {
 return (
-  <Layout title="RXP Guide" pageTitle="Quick Guide">
+  <Layout title="RXP Guide" pageTitle="RXP Guide">
+    <ImportGuide />
     <InitGuide />
     <ModifyTextGuide />
     <ConstructGuide />

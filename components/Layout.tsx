8957cbe06d1c8c5cc54ee6c24d258c3bdf838a93
Jeffrey Rose <jtr219@outlook.com>
2021-11-09 22:02:46 -0500

    replace badgen with shields badge

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,29 +3,32 @@ import { Head } from "./Head";
 import theme from "../styles/theme";
 import { Header } from "./Header";
 
-import { SiReact, SiNextDotJs, SiTypescript } from "react-icons/si"
+import { SiReact, SiNextDotJs, SiTypescript } from "react-icons/si";
 type IconTypes = typeof SiReact | typeof SiNextDotJs | typeof SiTypescript;
 
-const IconWithHover = ({Icon, linkPath}: {Icon: IconTypes, linkPath: string}) => {
+const IconWithHover = ({
+  Icon,
+  linkPath,
+}: {
+  Icon: IconTypes;
+  linkPath: string;
+}) => {
   const [isHovered, setHover] = useState(false);
   return (
     <a href={linkPath}>
-    <Icon 
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      color={isHovered ? theme.colors.background : "black"}
-    />
-    <style jsx>{`
-    a {
-      padding-left: .2em;
-    }
-    `}</style>
+      <Icon
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        color={isHovered ? theme.colors.background : "black"}
+      />
+      <style jsx>{`
+        a {
+          padding-left: 0.2em;
+        }
+      `}</style>
     </a>
-  )
+  );
 };
-
-const formattedThemeBKG = theme.colors.background.replace("#", "");
-const badgeSize = "1.1";
 
 type Props = {
   children?: ReactNode;
@@ -34,8 +37,10 @@ type Props = {
   sizing?: "standard" | "narrow" | "modest" | "large";
 };
 
-const getLayoutSizing = (sizing: "standard" | "narrow" | "modest" | "large") => {
-  switch(sizing) {
+const getLayoutSizing = (
+  sizing: "standard" | "narrow" | "modest" | "large"
+) => {
+  switch (sizing) {
     case "narrow":
       return "700";
     case "modest":
@@ -43,47 +48,54 @@ const getLayoutSizing = (sizing: "standard" | "narrow" | "modest" | "large") => 
     case "large":
       return "1200";
     default:
-      return "900"
+      return "900";
   }
 };
 
 const Layout = ({ children, pageTitle, title, sizing = "standard" }: Props) => (
   <div className="site-container">
-    
-    <Head title={title}/>
+    <Head title={title} />
     <Header />
     <div className="site-content">
-    <div className="site-badges">
-    <a href="https://www.github.com/jt-rose/rxp">
-    <img src={`https://badgen.net/github/release/jt-rose/rxp?icon=github&label&scale=${badgeSize}&color=${formattedThemeBKG}`} alt="Github"/>
-    </a>
-    <a href="https://www.npmjs.com/package/rxp">
-    <img src={`https://badgen.net/badge/icon/v1.4.5?icon=npm&label&scale=${badgeSize}&color=${formattedThemeBKG}`} alt="NPM"/>
-    </a>
-    <a href="https://www.typescriptlang.org/">
-    <img src={`https://badgen.net/badge/icon/typescript?icon=typescript&label&scale=${badgeSize}&color=${formattedThemeBKG}`} alt="TypeScript" />
-    </a>
-    </div>
+      <div className="site-badges">
+        <a href="https://www.github.com/jt-rose/rxp">
+          <img
+            src={`https://img.shields.io/badge/github-1.4.5-009879?logo=github`}
+            alt="Github"
+          />
+        </a>
+        <a href="https://www.npmjs.com/package/rxp">
+          <img
+            src={`https://img.shields.io/badge/npm-1.4.5-009879?logo=npm`}
+            alt="NPM"
+          />
+        </a>
+      </div>
       <main>
         <h1 className="nunito-font">{pageTitle}</h1>
-    {children}
-    </main>
+        {children}
+      </main>
 
-
-    <footer>
-      <div className="footer-spacing" />
-      <span>Developed by <a href="https://github.com/jt-rose" className="footer-link">
-        Jeff Rose
-        </a>, 2020</span>
-      <div className="techstack-icons">
-        <IconWithHover Icon={SiReact} linkPath="https://reactjs.org/" />
-        <IconWithHover Icon={SiNextDotJs} linkPath="https://nextjs.org/" />
-        <IconWithHover Icon={SiTypescript} linkPath="https://www.typescriptlang.org/" />
-      </div>
-    </footer>
-    
+      <footer>
+        <div className="footer-spacing" />
+        <span>
+          Developed by{" "}
+          <a href="https://github.com/jt-rose" className="footer-link">
+            Jeff Rose
+          </a>
+          , 2020
+        </span>
+        <div className="techstack-icons">
+          <IconWithHover Icon={SiReact} linkPath="https://reactjs.org/" />
+          <IconWithHover Icon={SiNextDotJs} linkPath="https://nextjs.org/" />
+          <IconWithHover
+            Icon={SiTypescript}
+            linkPath="https://www.typescriptlang.org/"
+          />
+        </div>
+      </footer>
     </div>
-    
+
     <style jsx>{`
       .site-container {
         display: flex;
@@ -100,12 +112,12 @@ const Layout = ({ children, pageTitle, title, sizing = "standard" }: Props) => (
         text-align: right;
       }
       .site-badges a {
-        padding-left: .2em;
+        padding-left: 0.2em;
       }
       .site-content {
         display: flex;
         flex-direction: column;
-        padding: 1em 1em .5em;
+        padding: 1em 1em 0.5em;
         flex-grow: 1;
       }
       main {
@@ -133,53 +145,54 @@ const Layout = ({ children, pageTitle, title, sizing = "standard" }: Props) => (
       }
     `}</style>
     <style jsx global>{`
-        html, body {
-          font-family: Roboto, sans-serif;
-          margin: 0;
-          padding: 0;
-          font-size: 18px;
-          font-weight: 400;
-          line-height: 1.8;
-          color: #333;
-        }
-        .nunito-font {
-          font-family: Nunito, sans-serif;
-        }
-        .link-styling {
-          text-decoration: none;
-          color: ${theme.colors.background};
-          font-weight: bold;
-        }
-        .link-styling:hover {
-          text-decoration: underline;
-        }
-        .guide-section {
-          display: flex;
-          flex-direction: column;
-          padding: 1em 1em 1.5em;
-        }
-        .section-title {
-          text-align: center;
-          margin-bottom: 0;
-        }
+      html,
+      body {
+        font-family: Roboto, sans-serif;
+        margin: 0;
+        padding: 0;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 1.8;
+        color: #333;
+      }
+      .nunito-font {
+        font-family: Nunito, sans-serif;
+      }
+      .link-styling {
+        text-decoration: none;
+        color: ${theme.colors.background};
+        font-weight: bold;
+      }
+      .link-styling:hover {
+        text-decoration: underline;
+      }
+      .guide-section {
+        display: flex;
+        flex-direction: column;
+        padding: 1em 1em 1.5em;
+      }
+      .section-title {
+        text-align: center;
+        margin-bottom: 0;
+      }
+      .sticky-header-adjust {
+        padding-top: 60px;
+        margin-top: -60px;
+      }
+      @media (min-width: ${theme.breakPoints.desktopWidth}) {
         .sticky-header-adjust {
-          padding-top: 60px; 
-          margin-top: -60px;
+          padding-top: 0;
+          margin-top: 0;
         }
-        @media (min-width: ${theme.breakPoints.desktopWidth}) {
-          .sticky-header-adjust {
-            padding-top: 0; 
-            margin-top: 0;
-          }
-        }
-        p .code-in-text {
-          color: #353535;
-          background-color: #f5f2f0;
-          border-radius: 3px;
-          padding: .2em .5em 0;
-          text-decoration: none;
-        }
-      `}</style>
+      }
+      p .code-in-text {
+        color: #353535;
+        background-color: #f5f2f0;
+        border-radius: 3px;
+        padding: 0.2em 0.5em 0;
+        text-decoration: none;
+      }
+    `}</style>
   </div>
 );
 
